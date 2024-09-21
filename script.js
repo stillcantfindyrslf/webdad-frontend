c18a21814b0daf5db9d4624d7553284e23208e55
fkpe <ilasavic511@gmail.com>
2024-09-21 22:28:37 +0300

    A-9: - add form

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.querySelectorAll('.emoji').forEach(emoji => {
         alert(`Спасибо за отзыв: ${emoji.alt}`)
     })
 })
+
+document.getElementById('email-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    this.reset();
+})
